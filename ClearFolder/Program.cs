b3493eb81654cfa529e404c8b270457926571cce
Julia Aasmaa <my_e_10@hotmail.com>
2020-11-18 15:47:10 +0200

    commit

---
+++ ClearFolder/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+
+namespace ClearFolder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootPath = @"C:\Users\julia.voronetskaja\samples";
+            DeleteAllfiles();
+
+            DirectoryInfo rootDirectory = new DirectoryInfo(rootPath);
+            foreach(DirectoryInfo dir in rootDirectory.GetDirectories())
+            {
+                DeleteAllfolders(dir.FullName, true);
+            }
+
+           
+        }
+
+        //a function to delete all the files
+        public static void DeleteAllfiles()
+        {
+            string rootPath = @"C:\Users\julia.voronetskaja\samples";
+            DirectoryInfo directory = new DirectoryInfo(rootPath);
+
+            foreach(FileInfo file in directory.GetFiles())
+            {
+                file.Delete();
+            }
+        }
+        //a function to delete all the folders
+        public static void DeleteAllfolders(string directoryName, bool ifExists)
+        {
+            if (Directory.Exists(directoryName))
+            {
+                Directory.Delete(directoryName, true);
+            } else if (ifExists)
+            {
+                throw new SystemException("No such directory to delete");
+            }
+        }
+    }
+}
---
+++ CreateDirectory/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace CreateDirectory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateFolderOne();
+        }
+
+        public static void CreateFolderOne()
+        {
+            string rootPath = @"C:\Users\julia.voronetskaja\samples";
+            string folderName = "playerOne";
+            string fullFolderPath = Path.Combine(rootPath, folderName);
+            Console.WriteLine(fullFolderPath);
+            if (Directory.Exists(fullFolderPath)) //check if folder exists
+            {
+                Console.WriteLine("The folder already exists.");
+            } else
+            {
+                Directory.CreateDirectory(fullFolderPath);
+            }
+        }
+
+        public static void CreateFolderTwo()
+        {
+
+        }
+    }
+}
